9b68752f85ef6c2e321b3cea3092319b60ceb963
rameezrz <76533218+rameezrz@users.noreply.github.com>
2022-11-20 00:12:52 +0530

    Added file

--- backend/routes/api/titles.js
+++ backend/routes/api/titles.js
@@ -2,12 +2,11 @@ var router = require('express').Router();
 var mongoose = require('mongoose');
 var Item = mongoose.model('Item');
 
-// return a list of tags
+// return a list of titles
 router.get('/', function(req, res, next) {
-  Item.find().distinct('tagList').then(function(tags){
-    return res.json({tags: tags});
+  Item.find().distinct('titleList').then(function(title){
+    return res.json({title: title});
   }).catch(next);
 });
-
 
 module.exports = router;
